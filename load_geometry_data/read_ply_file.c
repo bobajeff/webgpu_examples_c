847a81492a2dcbb35919d724ad60932726740e76
Jeffrey Brusseau <bobajeff82@gmail.com>
2023-10-26 13:00:47 -0500

    pass pointer data to callbacks instead of using globals in read_ply_file

--- load_geometry_data/read_ply_file.c
+++ load_geometry_data/read_ply_file.c
@@ -2,64 +2,80 @@
 #include <stdlib.h>
 #include "rply.h"
 
-float *_vertexData;
-unsigned char *colorData;
+typedef struct vertex_pointers {
+  float *vertexData;
+  unsigned char *colorData;
+} vertex_pointers;
 
 static int x_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  _vertexData[instance_index * 4] = (float)ply_get_argument_value(argument);
+  v->vertexData[instance_index * 4] = (float)ply_get_argument_value(argument);
   return 1;
 }
 
 static int y_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  _vertexData[instance_index * 4 + 1] = (float)ply_get_argument_value(argument);
+  v->vertexData[instance_index * 4 + 1] = (float)ply_get_argument_value(argument);
   return 1;
 }
 
 static int z_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  _vertexData[instance_index * 4 + 2] = (float)ply_get_argument_value(argument);
+  v->vertexData[instance_index * 4 + 2] = (float)ply_get_argument_value(argument);
   return 1;
 }
 
 static int r_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  colorData[instance_index * 16 + 12] = (unsigned char)ply_get_argument_value(argument);
+  v->colorData[instance_index * 16 + 12] = (unsigned char)ply_get_argument_value(argument);
   return 1;
 }
 
 static int g_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  colorData[instance_index * 16 + 12 + 1] =
+  v->colorData[instance_index * 16 + 12 + 1] =
       (unsigned char)ply_get_argument_value(argument);
   return 1;
 }
 
 static int b_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  colorData[instance_index * 16 + 12 + 2] =
+  v->colorData[instance_index * 16 + 12 + 2] =
       (unsigned char)ply_get_argument_value(argument);
   return 1;
 }
 
 static int a_cb(p_ply_argument argument) {
   long instance_index;
+  vertex_pointers *v;
+  ply_get_argument_user_data(argument, (void **)&v, NULL);
   ply_get_argument_element(argument, NULL, &instance_index);
 
-  colorData[instance_index * 16 + 12 + 3] =
+  v->colorData[instance_index * 16 + 12 + 3] =
       (unsigned char)ply_get_argument_value(argument);
   return 1;
 }
@@ -75,17 +91,18 @@ int get_vertex_data(const char *ply_filepath, float **vertexData, int *vertexDat
     fprintf(stderr, "Can't read ply header\n");
     return 1;
   }
-  *numVertices = ply_set_read_cb(ply, "vertex", "x", x_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "y", y_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "z", z_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "red", r_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "green", g_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "blue", b_cb, NULL, 0);
-  ply_set_read_cb(ply, "vertex", "alpha", a_cb, NULL, 0);
+  vertex_pointers *v;
+  *numVertices = ply_set_read_cb(ply, "vertex", "x", x_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "y", y_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "z", z_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "red", r_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "green", g_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "blue", b_cb, v, 0);
+  ply_set_read_cb(ply, "vertex", "alpha", a_cb, v, 0);
   *vertexDataSize = *numVertices * 4 * sizeof(float);
   *vertexData = (float *)malloc(*vertexDataSize);
-  _vertexData = *vertexData;
-  colorData = (unsigned char *)*vertexData;
+  v->vertexData = *vertexData;
+  v->colorData = (unsigned char *)*vertexData;
   if (!ply_read(ply)) {
     fprintf(stderr, "Can't read element/property data from ply file\n");
     return 1;
