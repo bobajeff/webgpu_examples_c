d76c9c48f03af9a72caf2d00d721d82e1ba62513
Jeffrey Brusseau <bobajeff82@gmail.com>
2023-11-24 10:17:57 -0600

    chapter 'uv coordinates' of  'An introduction to Shader Art Coding'

---
+++ shadertoy/res/shader_art_coding_introduction.wgsl
@@ -0,0 +1,25 @@
+// WebGPU and C port of  'An introduction to Shader Art Coding' @ https://www.youtube.com/watch?v=f4s1h2YETNY & https://www.shadertoy.com/view/mtyGWy
+@group(0) @binding(0) var<uniform> iResolution: vec2<f32>;
+
+@vertex fn vs(
+@builtin(vertex_index) vertexIndex : u32
+) -> @builtin(position) vec4<f32> {
+    var pos = array<vec2<f32>, 6>(
+        vec2<f32>( -1.0,  1.0),  // top left
+        vec2<f32>(-1.0, -1.0),  // bottom left
+        vec2<f32>( 1.0, -1.0),   // bottom right
+        vec2<f32>( -1.0,  1.0),  // top left
+        vec2<f32>( 1.0, -1.0),   // bottom right
+        vec2<f32>(1.0, 1.0),  // top right
+    );
+
+    return vec4<f32>(pos[vertexIndex], 0.0, 1.0);
+}
+
+@fragment fn fs(@builtin(position) position: vec4<f32>) -> @location(0) vec4<f32> {
+    var uv = vec2<f32>();
+    uv.x = position.x / iResolution.x;
+    uv.y = 1.0 - (position.y / iResolution.y); // invert y
+
+    return vec4<f32>(uv.x, uv.y, 0.0, 1.0);
+}
---
+++ shadertoy/shader_art_coding_introduction.c
@@ -0,0 +1,232 @@
+// WebGPU and C port of  'An introduction to Shader Art Coding' @ https://www.youtube.com/watch?v=f4s1h2YETNY & https://www.shadertoy.com/view/mtyGWy
+#include "create_surface.h"
+#include "framework.h"
+#include "webgpu.h"
+#include <GLFW/glfw3.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  initializeLog();
+
+  WGPUInstance instance =
+      wgpuCreateInstance(&(WGPUInstanceDescriptor){.nextInChain = NULL});
+
+  WGPUAdapter adapter;
+  wgpuInstanceRequestAdapter(instance, NULL, request_adapter_callback,
+                             (void *)&adapter);
+
+  WGPUDevice device;
+  wgpuAdapterRequestDevice(adapter, NULL, request_device_callback,
+                           (void *)&device);
+
+  WGPUQueue queue = wgpuDeviceGetQueue(device);
+
+  wgpuDeviceSetUncapturedErrorCallback(device, handle_uncaptured_error, NULL);
+
+  // Create GLFW Window and use as WebGPU surface
+  if (!glfwInit()) {
+    printf("Cannot initialize glfw");
+    return 1;
+  }
+
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "wgpu with glfw", NULL, NULL);
+
+  if (!window) {
+    printf("Cannot create window");
+    return 1;
+  }
+  WGPUSurface surface = create_surface(instance, window);
+  WGPUTextureFormat presentationFormat =
+      wgpuSurfaceGetPreferredFormat(surface, adapter);
+
+  WGPUShaderModuleDescriptor shaderSource =
+      load_wgsl(RESOURCE_DIR "shader_art_coding_introduction.wgsl");
+  WGPUShaderModule module = wgpuDeviceCreateShaderModule(device, &shaderSource);
+
+  WGPURenderPipeline pipeline = wgpuDeviceCreateRenderPipeline(
+      device,
+      &(WGPURenderPipelineDescriptor){
+          .label = "our hardcoded red triangle pipeline",
+          .vertex =
+              (WGPUVertexState){
+                  .module = module,
+                  .entryPoint = "vs",
+                  .bufferCount = 0,
+                  .buffers = NULL,
+              },
+          .primitive =
+              (WGPUPrimitiveState){
+                  .topology = WGPUPrimitiveTopology_TriangleList,
+                  .stripIndexFormat = WGPUIndexFormat_Undefined,
+                  .frontFace = WGPUFrontFace_CCW,
+                  .cullMode = WGPUCullMode_Back},
+          .multisample =
+              (WGPUMultisampleState){
+                  .count = 1,
+                  .mask = ~0,
+                  .alphaToCoverageEnabled = false,
+              },
+          .fragment =
+              &(WGPUFragmentState){
+                  .module = module,
+                  .entryPoint = "fs",
+                  .targetCount = 1,
+                  .targets =
+                      &(WGPUColorTargetState){
+                          .format = presentationFormat,
+                          .blend =
+                              &(WGPUBlendState){
+                                  .color =
+                                      (WGPUBlendComponent){
+                                          .srcFactor = WGPUBlendFactor_One,
+                                          .dstFactor = WGPUBlendFactor_Zero,
+                                          .operation = WGPUBlendOperation_Add,
+                                      },
+                                  .alpha =
+                                      (WGPUBlendComponent){
+                                          .srcFactor = WGPUBlendFactor_One,
+                                          .dstFactor = WGPUBlendFactor_Zero,
+                                          .operation = WGPUBlendOperation_Add,
+                                      }},
+                          .writeMask = WGPUColorWriteMask_All,
+                      },
+              },
+          .depthStencil = NULL,
+      });
+
+  WGPUSwapChainDescriptor config = (WGPUSwapChainDescriptor){
+      .nextInChain =
+          (const WGPUChainedStruct *)&(WGPUSwapChainDescriptorExtras){
+              .chain =
+                  (WGPUChainedStruct){
+                      .next = NULL,
+                      .sType = (WGPUSType)WGPUSType_SwapChainDescriptorExtras,
+                  },
+              .alphaMode = WGPUCompositeAlphaMode_Auto,
+              .viewFormatCount = 0,
+              .viewFormats = NULL,
+          },
+      .usage = WGPUTextureUsage_RenderAttachment,
+      .format = presentationFormat,
+      .width = 0,
+      .height = 0,
+      .presentMode = WGPUPresentMode_Fifo,
+  };
+
+  WGPUBuffer uniform_buffer;
+  WGPUBindGroup bind_group;
+  float iResolution[2];
+  const uint64_t uniform_buffer_size = sizeof(iResolution);
+
+  // resolution
+  uniform_buffer = wgpuDeviceCreateBuffer(
+      device, &(WGPUBufferDescriptor){.label = "uniforms",
+                                      .size = uniform_buffer_size,
+                                      .usage = WGPUBufferUsage_Uniform |
+                                               WGPUBufferUsage_CopyDst,
+                                      .mappedAtCreation = false});
+
+  bind_group = wgpuDeviceCreateBindGroup(
+      device,
+      &(WGPUBindGroupDescriptor){
+          .label = "bind group for object",
+          .layout = wgpuRenderPipelineGetBindGroupLayout(pipeline, 0),
+          .entries =
+              (WGPUBindGroupEntry[]){
+                  {.binding = 0,
+                   .buffer = uniform_buffer,
+                   .size = uniform_buffer_size,
+                   .offset = 0},
+              },
+          .entryCount = 1});
+
+  glfwGetWindowSize(window, (int *)&config.width, (int *)&config.height);
+
+  WGPUSwapChain swapChain = wgpuDeviceCreateSwapChain(device, surface, &config);
+
+  while (!glfwWindowShouldClose(window)) {
+
+    WGPUTextureView view = NULL;
+
+    for (int attempt = 0; attempt < 2; attempt++) {
+      uint32_t prevWidth = config.width;
+      uint32_t prevHeight = config.height;
+      glfwGetWindowSize(window, (int *)&config.width, (int *)&config.height);
+
+      if (prevWidth != config.width || prevHeight != config.height) {
+        swapChain = wgpuDeviceCreateSwapChain(device, surface, &config);
+      }
+      // Get the current texture from the swapChain to use for rendering to by the render pass
+      view = wgpuSwapChainGetCurrentTextureView(swapChain);
+      if (attempt == 0 && !view) {
+        printf("wgpuSwapChainGetCurrentTextureView() failed; trying to create "
+               "a new swap chain...\n");
+        config.width = 0;
+        config.height = 0;
+        continue;
+      }
+
+      break;
+    }
+
+    if (!view) {
+      printf("Cannot acquire next swap chain texture\n");
+      return 1;
+    }
+
+    // make a command encoder to start encoding commands
+    WGPUCommandEncoder encoder = wgpuDeviceCreateCommandEncoder(
+        device, &(WGPUCommandEncoderDescriptor){.label = "our encoder"});
+
+    WGPURenderPassDescriptor renderPassDescriptor = {
+        .colorAttachments =
+            &(WGPURenderPassColorAttachment){
+                .view = view, //texture from SwapChain
+                .resolveTarget = NULL,
+                .loadOp = WGPULoadOp_Clear,
+                .storeOp = WGPUStoreOp_Store,
+                .clearValue =
+                    (WGPUColor){
+                        .r = 0.3,
+                        .g = 0.3,
+                        .b = 0.3,
+                        .a = 1.0,
+                    },
+            },
+        .colorAttachmentCount = 1,
+        .depthStencilAttachment = NULL,
+    };
+    int window_width, window_height;
+
+    glfwGetWindowSize(window, (int *)&window_width, (int *)&window_height);
+    iResolution[0] = window_width;
+    iResolution[1] = window_height;
+
+    wgpuQueueWriteBuffer(queue, uniform_buffer, 0, &iResolution,
+                         sizeof(iResolution));
+    // make a render pass encoder to encode render specific commands
+    WGPURenderPassEncoder pass =
+        wgpuCommandEncoderBeginRenderPass(encoder, &renderPassDescriptor);
+    wgpuRenderPassEncoderSetPipeline(pass, pipeline);
+    wgpuRenderPassEncoderSetBindGroup(pass, 0, bind_group, 0, NULL);
+    wgpuRenderPassEncoderDraw(pass, 6, 1, 0,
+                              0); // call our vertex shader 3 times.
+    wgpuRenderPassEncoderEnd(pass);
+
+    WGPUQueue queue = wgpuDeviceGetQueue(device);
+    WGPUCommandBuffer commandBuffer = wgpuCommandEncoderFinish(
+        encoder, &(WGPUCommandBufferDescriptor){.label = NULL});
+    wgpuQueueSubmit(queue, 1, &commandBuffer);
+    wgpuSwapChainPresent(swapChain);
+
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
+  return 0;
+}
